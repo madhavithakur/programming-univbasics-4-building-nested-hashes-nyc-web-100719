6ea79c486cee912bb7c651339a335bfdc35bdaba
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-28 02:24:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,9 +5,9 @@ end
 def monopoly_with_second_tier
   monopoly = {
     :railroad => 
-    {:pieces => 4, 
-    :rent_in_dollars =>{} 
-      
+    {
+      :pieces => 4, 
+      :rent_in_dollars => {} 
     }
   }
 end
