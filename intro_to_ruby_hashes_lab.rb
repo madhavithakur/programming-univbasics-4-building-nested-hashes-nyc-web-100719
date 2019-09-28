24f4e870ed944e3ad51e70b7aca82456c56741a9
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-28 03:05:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,13 +27,8 @@ def monopoly_with_third_tier
     {
       :reading_railroad => {},
       :pennsylvania_railroad => {},
-      
-      :b_and_o_railroad=>
-      
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
+      :b_and_o_railroad => {},
+      :shortline => {}
     }
   
 end
